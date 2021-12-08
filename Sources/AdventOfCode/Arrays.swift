cb850c1e1571a20584cd935aa03a64a142e72194
Peter VanLund <792688+petermeansrock@users.noreply.github.com>
2021-12-07 22:45:58 -0800

    fix: make Array extension's methods public

--- Sources/AdventOfCode/Arrays.swift
+++ Sources/AdventOfCode/Arrays.swift
@@ -4,7 +4,7 @@ extension Array where Element: Collection, Element.Index == Int {
     /// Returns a column of a row-major, multi-dimensional  array.
     /// - Parameter index: The zero-based index of the column to return.
     /// - Returns: A column of a row-major, multi-dimensional  array.
-    func column(at index: Element.Index) -> [Element.Iterator.Element] {
+    public func column(at index: Element.Index) -> [Element.Iterator.Element] {
         return self.map { $0[index] }
     }
 
@@ -14,7 +14,7 @@ extension Array where Element: Collection, Element.Index == Int {
     /// multi-dimensional array. Effectively, this means that this method assumes that the first two
     /// dimensions of the multi-dimensional array are rectangular.
     /// - Returns: The columns of a row-major, multi-dimensional  array.
-    func columns() -> [[Element.Iterator.Element]] {
+    public func columns() -> [[Element.Iterator.Element]] {
         return (0..<self[0].count).map { self.column(at: $0) }
     }
 }
