0a8773660fbc29cc42a8be3043a3c9453dd1afe5
Peter VanLund <792688+petermeansrock@users.noreply.github.com>
2021-12-07 22:02:10 -0800

    test: remove day-specific geometry tests

--- Tests/AdventOfCodeTests/GeometryTests.swift
+++ Tests/AdventOfCodeTests/GeometryTests.swift
@@ -252,25 +252,6 @@ class PlotTests: XCTestCase {
         XCTAssertEqual(overlappingCount, 5)
     }
 
-    func testPlotWithDay5InputIgnoringDiagonals() throws {
-        // Arrange
-        let lines = InputFile(day: 5).loadLines().filter { $0.count > 0 }
-        let segments = lines.map { try! LineSegment(in: $0) }.filter { $0.orientation != .diagonal }
-        let coordinates = segments.flatMap { [$0.start, $0.end] }
-        let maxX = coordinates.map { $0.x }.max()!
-        let maxY = coordinates.map { $0.y }.max()!
-
-        // Act
-        var plot = Plot(maxX: maxX, maxY: maxY)
-        for segment in segments {
-            plot.plot(line: segment)
-        }
-
-        // Assert
-        let overlappingCount = plot.grid.flatMap { $0 }.filter { $0 >= 2 }.count
-        XCTAssertEqual(overlappingCount, 7269)
-    }
-
     func testPlotWithSampleInputConsideringDiagonals() throws {
         // Arrange
         let lines = [
@@ -299,24 +280,5 @@ class PlotTests: XCTestCase {
         // Assert
         let overlappingCount = plot.grid.flatMap { $0 }.filter { $0 >= 2 }.count
         XCTAssertEqual(overlappingCount, 12)
-    }
-
-    func testPlotWithDay5InputConsideringDiagonals() throws {
-        // Arrange
-        let lines = InputFile(day: 5).loadLines().filter { $0.count > 0 }
-        let segments = lines.map { try! LineSegment(in: $0) }
-        let coordinates = segments.flatMap { [$0.start, $0.end] }
-        let maxX = coordinates.map { $0.x }.max()!
-        let maxY = coordinates.map { $0.y }.max()!
-
-        // Act
-        var plot = Plot(maxX: maxX, maxY: maxY)
-        for segment in segments {
-            plot.plot(line: segment)
-        }
-
-        // Assert
-        let overlappingCount = plot.grid.flatMap { $0 }.filter { $0 >= 2 }.count
-        XCTAssertEqual(overlappingCount, 21140)
     }
 }
